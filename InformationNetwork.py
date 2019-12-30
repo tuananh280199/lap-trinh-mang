52a0d01a665b8c55edd5e96ff8bd4325baf83556
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2019-12-30 20:18:58 +0700

    add file

---
+++ InformationNetwork.py
@@ -0,0 +1,13 @@
+import netifaces
+if __name__ == '__main__':
+	ntf = netifaces.interfaces()
+	for i in ntf:
+		ipaddr = netifaces.ifaddresses(i)
+		if netifaces.AF_INET in ipaddr :
+			dcip = ipaddr[netifaces.AF_INET]
+			dcip = dcip[0]
+			print("Giao gien mang {0}".format(i))
+			print("IP address : {0}".format(dcip['addr']))
+			print("Subnet mask : {0}".format(dcip['netmask']))
+			gateway = netifaces.gateways()
+			print("Defaunt gateways : ",gateway['defaunt'][netifaces.AF_INET[0]])
